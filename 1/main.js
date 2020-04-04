bb7c50cb71b43a0bc81dc8a0a22f5b56c8e36c9a
vapvin <tpehdj12@daum.net>
2020-04-04 20:55:51 +0900

    Function -02

--- 1/main.js
+++ 1/main.js
@@ -10,3 +10,72 @@ var val = add(5, 4); // val 에 add함수의 결과를 담는다.
 console.log(val)
 
 // ------------------------------------------------- //
+
+var test = 2 // 대입연산자 < 값을 넣을때 사용
+
+function FunctionName() {
+    // 코드 
+}
+
+// {} << 코드블럭 
+
+// ------------------------------------------------- //
+
+function hello(){
+    console.log("Hello");
+}
+
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+
+// ------------------------------------------------- //
+
+// Arow Function
+
+const name1 = () => {
+    console.log("Arow Function")
+}
+
+name1();
+name1();
+name1();
+name1();
+name1();
+name1();
+
+// ---------------------------------------------- //
+
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+
+window.alert() // method
+
+C 구조체 
+
+C++ 클래스 
+
+Python class
+
+java class
+
+Java Object class
+
+Go 구조체
+
+Rust 클래스
